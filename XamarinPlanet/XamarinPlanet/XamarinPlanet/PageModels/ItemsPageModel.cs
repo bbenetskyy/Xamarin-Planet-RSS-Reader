c4c0705c7713ecdaed91d85f147a298cccbf1d53
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2022-06-13 17:22:59 +0200

    resolve conflicts

--- XamarinPlanet/XamarinPlanet/XamarinPlanet/PageModels/ItemsPageModel.cs
+++ XamarinPlanet/XamarinPlanet/XamarinPlanet/PageModels/ItemsPageModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using MvvmCross.Commands;
 using MvvmCross.Navigation;
@@ -10,7 +11,11 @@ namespace XamarinPlanet
 {
     public class ItemsPageModel : BasePageModel
     {
+        const string PUB_DATE_DESC = "Publication date descending";
+        const string PUB_DATE_ASC = "Publication date ascending";
+
         private readonly IRssClient _rssClient;
+        private string _sortBy;
 
         public ItemsPageModel(
             ILogger logger,
@@ -20,6 +25,19 @@ namespace XamarinPlanet
             _rssClient = rssClient;
             Items = new MvxObservableCollection<Item>();
             ChooseItemCommand = new MvxAsyncCommand<Item>(ExecuteChooseItemCommand);
+            _sortBy = PUB_DATE_DESC;
+        }
+
+        public string[] SortBySource => new string[] { PUB_DATE_DESC, PUB_DATE_ASC };
+        public string SortBy
+        {
+            get => _sortBy;
+            set => SetProperty(ref _sortBy, value, () =>
+            {
+                var items = Items?.ToArray();
+                Items.Clear();
+                Items.AddRange(OrderBy(_sortBy, items));
+            });
         }
 
         public MvxObservableCollection<Item> Items { get; }
@@ -34,7 +52,7 @@ namespace XamarinPlanet
                 var contributors = await _rssClient.DownloadItems<Item>(RssClientConstants.ITEM_RSS_KEY);
                 await Device.InvokeOnMainThreadAsync(() =>
                 {
-                    Items.AddRange(contributors);
+                    Items.AddRange(OrderBy(SortBy, contributors?.ToArray()));
                 });
             }
             catch (Exception ex)
@@ -50,6 +68,19 @@ namespace XamarinPlanet
         private Task ExecuteChooseItemCommand(Item item)
         {
             return MvxNavigationService.Navigate<ItemDetailsPageModel, Item>(item);
+        }
+
+
+        private Item[] OrderBy(string orderBy, Item[] items)
+        {
+            switch (orderBy)
+            {
+                case PUB_DATE_DESC:
+                    return items?.OrderByDescending(x => x.PubDate)?.ToArray();
+                case PUB_DATE_ASC:
+                default:
+                    return items?.OrderBy(x => x.PubDate)?.ToArray();
+            }
         }
 
     }
