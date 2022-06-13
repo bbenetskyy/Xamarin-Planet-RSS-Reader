73c4837d153ef5fa9c1e906a38b554a7c64b5b09
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2022-06-13 17:17:56 +0200

    iOS Save Area

--- XamarinPlanet/XamarinPlanet/XamarinPlanet/Pages/MainPage.xaml.cs
+++ XamarinPlanet/XamarinPlanet/XamarinPlanet/Pages/MainPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using MvvmCross.Forms.Views;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace XamarinPlanet
 {
@@ -14,6 +16,14 @@ namespace XamarinPlanet
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            On<iOS>().SetUseSafeArea(false);
+            var safeAreaInsets = On<iOS>().SafeAreaInsets();
+            HeaderFrame.Padding = new Thickness(24, 24 + safeAreaInsets.Top, 24, 24);
         }
     }
 }
